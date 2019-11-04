e0c5a477bf963d69958c090edcc0e3ad1c20fdf7
AlexeySinitsin1981 <20035@list.ru>
2019-11-04 11:30:31 +0300

    Изменил реализацию удаления, обновления и ключа

--- src/main/java/task21/MyMap.java
+++ src/main/java/task21/MyMap.java
@@ -1,55 +1,69 @@
 package task21;
 
-import java.util.HashMap;
-
 public class MyMap {
     private static final int SIZE = 16;
 
-    private Entry table[] = new Entry[SIZE];
+    private Entry[] table= new Entry[SIZE];
 
-    int count=0;
+    int count = 0;
 
 
-    public Object get(Object k) {
+     Object get(Object k) {
         int column = k.hashCode() % SIZE;
         Entry e = table[column];
 
-        while(e != null) {
-            if(e.key.equals(k)) {
+        while (e != null) {
+            if (e.key.equals(k)) {
                 return e;
             }
             e = e.next;
         }
-        return null;
+        return "Объект не существует";
     }
 
-    public Object remove(Object k) {
+     boolean remove(Object k) {
         int column = k.hashCode() % SIZE;
         Entry e = table[column];
-        while(e!=null) {
-        if(e.key.equals(k)) {
-            count--;
-            return table[column]=null;
-        }
-            table[column]=null;
-        }
+        if (table[column] == null) {
+            return false;
+        } else {
+            Entry previous = null;
+            Entry current = table[column];
 
-        return table[column];
+            while (current != null) {
+                if (current.key.equals(k)) {
+                    if (previous == null) {
+                        table[column] = table[column].next;
+                        count--;
+                        return true;
+
+                    } else {
+                        previous.next = current.next;
+                        return true;
+                    }
+                }
+                previous = current;
+                current = current.next;
+            }
+            return false;
+        }
     }
 
-    public void add(Object k, Object v) {
+     void add(Object k, Object v) {
+        if (k == null)
+            return;
         int column = k.hashCode() % SIZE;
         Entry e = table[column];
 
-        if(e != null) {
-            if(e.key.equals(k)) {
+        if (e != null) {
+            if (e.key.equals(k)) {
                 e.value = v;
             } else {
 
-                while(e.next != null) {
+                while (e.next != null) {
                     e = e.next;
                 }
-                e.next =  new Entry(k, v);
+                e.next = new Entry(k, v);
             }
         } else {
             Entry entryNewBucket = new Entry(k, v);
@@ -59,53 +73,61 @@ public class MyMap {
     }
 
 
-    public boolean update(Object k, Object v) {
+     void update(Object k, Object v) {
         int column = k.hashCode() % SIZE;
         Entry e = table[column];
 
-        while (table[column].key.equals(k)) {
-                table[column] = new Entry(k, v);
-                return true;
-        }
-        return false;
-    }
+        if (e != null) {
+            if (e.key.equals(k)) {
+                e.value = v;
+            } else {
 
-    public boolean checking(Object k) {
-        int column = k.hashCode() % SIZE;
-        Entry e = table[column];
-        while(e != null) {
-            if(e.key.equals(k)) {
-                return true;
+                while (e.next != null) {
+                    e = e.next;
+                }
+                e.next = new Entry(k, v);
             }
+        } else {
+            Entry entryNewBucket = new Entry(k, v);
+            table[column] = entryNewBucket;
+        }
+    }
+
+
+    private boolean checking(Object k) {
+        int column = k.hashCode() % SIZE;
+        Entry e = table[column];
+        if (e != null) {
+            return e.key.equals(k);
         }
         return false;
     }
 
-
-
-      public int getCount() {
+    private int getCount() {
         return count;
     }
 
-        public static void main (String[]args){
-            MyMap myHashMap = new MyMap();
+    public static void main (String[]args){
+        MyMap myHashMap = new MyMap();
 
-            myHashMap.add("Audi", "Germany");
-            myHashMap.add("Land-Rover", "England");
-            myHashMap.add("Lada", "Russia");
-            myHashMap.add("Fiat", "Italy");
-            myHashMap.add("Ford", "USA");
+        myHashMap.add("Audi", "Germany");
+        myHashMap.add("Land-Rover", "England");
+        myHashMap.add("Lada", "Russia");
+        myHashMap.add("Fiat", "Italy");
+        myHashMap.add("Ford", "USA");
 
-            System.out.println("" + myHashMap.get("Lada"));
-            myHashMap.remove("Lada");
-            System.out.println("" + myHashMap.get("Lada"));
-            System.out.println(myHashMap.getCount());
-            myHashMap.update("Land-Rover", "Cyprus");
-            System.out.println("" + myHashMap.get("Land-Rover"));
-            System.out.println(myHashMap.checking("Ford"));
+        System.out.println("" + myHashMap.get("Lada"));
+        myHashMap.remove("Lada");
+        System.out.println("" + myHashMap.get("Lada"));
 
 
-        }
+        System.out.println(myHashMap.getCount());
+        myHashMap.update("Land-Rover", "Cyprus");
+        System.out.println("" + myHashMap.get("Land-Rover"));
+        System.out.println(myHashMap.checking("Ford"));
+
+
     }
+}
 
 
